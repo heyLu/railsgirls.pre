39de8b1988eeeacf7db29dc7d186fb9f4ff20af1
Lucas Stadler <mi.au@papill0n.org>
2013-07-31 18:14:46 +0200

    Rails template generated by rails new FANCYNESS.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppPre::Application.config.secret_key_base = '55f87c3eec0d5f82c0d1cd1ab7acf0394e6886f0cba5d11278dce70a9744f44eabee311c2eb7d7515a32f59ea3315a2337d6fcb67951944d94870038fba9eed8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AppPre::Application.config.session_store :cookie_store, key: '_app_pre_session'
